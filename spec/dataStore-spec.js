5193f98234cd4041d1449e99635c9d9b7a229041
Chimney42 <draggili@gmail.com>
2017-12-04 17:38:34 +0100

    transforming test data

--- spec/dataStore-spec.js
+++ spec/dataStore-spec.js
@@ -57,5 +57,37 @@ describe('The dataStore', () => {
                 })
                 .catch(console.error)
         });
+
+        it('should transform each row', (done) => {
+            const target = 0;
+            const input = [1, 2, 3];
+
+            spyOn(readStreamMock, 'on').and.callFake((type, cb) => {
+                if ('data' === type) cb([target].concat(input));
+                if ('end' === type) cb();
+                return readStreamMock;
+            });
+
+            store.loadTrainingData()
+                .then(data => {
+                    expect(Array.from(data.inputData[0].getData().values)).toEqual(input);
+                    expect(data.targetData[0]).toEqual(store.createVectorRepresentation(10, target));
+                    done();
+                })
+        })
+    });
+    it('should do one-hot-encoding on labels', (done) => {
+        const label = 4;
+        const vector = store.createVectorRepresentation(10, label);
+        vector.data()
+            .then(data => {
+                expect(data[label]).toBeTruthy();
+                let random = label;
+                while (random === label) {
+                    random = Math.floor(Math.random() * 10);
+                }
+                expect(data[random]).toBeFalsy();
+                done();
+            })
     })
 });
--- src/dataStore.js
+++ src/dataStore.js
@@ -1,3 +1,4 @@
+const deeplearn = require('deeplearn');
 const parse = require('csv-parse');
 class DataStore {
     constructor() {
@@ -11,6 +12,16 @@ class DataStore {
         };
     }
 
+    createVectorRepresentation(length, index) {
+        const data = [];
+        for (let i = 0; i < length; i ++) {
+            data.push(false);
+        }
+        data[index] = true;
+        return deeplearn.Array1D.new(data);
+    };
+
+
     loadTrainingData() {
         return new Promise((resolve, reject) => {
             if (this.trainingData.init) {
@@ -19,7 +30,17 @@ class DataStore {
                 this.fs.createReadStream(this.trainingDataPath)
                     .pipe(parse({delimiter: ','}))
                     .on('data', (csvrow) => {
+                        const target = csvrow.shift();
+                        if ('label' != target) {
+                            this.trainingData.targetData.push(this.createVectorRepresentation(10, parseInt(target)));
 
+                            const inputRow = deeplearn.Array1D.new(csvrow);
+                            this.trainingData.inputData.push(inputRow);
+
+                            if (this.trainingData.inputData.length % 1000 === 0) {
+                                console.log(`storing datapoint ${this.trainingData.inputData.length} for training`)
+                            }
+                        }
                     })
                     .on('end', () => {
                         this.trainingData.init = true;
